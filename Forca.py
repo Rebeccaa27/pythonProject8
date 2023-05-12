bf75be0d9b35a0c93d1af09d595c5862ef1433d3
Rebeca <rebecca.alves2724@gmail.com>
2023-05-12 05:45:47 -0300

    Jogos

--- Forca.py
+++ Forca.py
@@ -1,8 +1,26 @@
-
-def jogar() :
+def jogar():
     print('################################')
     print('    Bem vindo o jogo da Forca   ')
     print('################################')
+
+    palavra_secreta = 'morango'
+    enforcou = False
+    acertou = False
+
+
+    while(not enforcou and not acertou):
+
+        chute = input('Qual letra? ')
+
+        index = 1
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print('Encontrei a letra {} na posição: {}'.format(letra, index))
+            index = index + 1
+
+
+        print(' jogando ...')
+
 
     print('Fim de Jogo ')
 
